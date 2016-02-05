f7a4520f02ecdb54e857414fa6cc49fa4e03709a
ItzProxy <allure.success@gmail.com>
2016-02-04 21:18:59 -0600

    Initial

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.f_name = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Status: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(241, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "D/L Folder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // f_name
+            // 
+            this.f_name.AutoSize = true;
+            this.f_name.Location = new System.Drawing.Point(12, 131);
+            this.f_name.Name = "f_name";
+            this.f_name.Size = new System.Drawing.Size(0, 13);
+            this.f_name.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(210, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 161);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.f_name);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Youtube Music";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label f_name;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string folder;
+        public string d_status = "Status: ";
+        public bool downloaded = false;
+        public bool answer;
+        public Form1()
+        {
+            InitializeComponent();
+            label2.MaximumSize = new Size(344, 0);
+            f_name.MaximumSize = new Size(344, 0);
+            label2.AutoSize = true;
+            f_name.AutoSize = true;
+            folder = GetDefaultFolder();
+            f_name.Text = folder;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = d_status + "Please enter the URL into the text box below";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //does nothing
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string URL = textBox1.Text;
+            if (URL.Trim().StartsWith("https://www.youtube.com/watch?v="))
+            {
+                using (var service = Client.For(new YouTube()))
+                {
+                    label2.Text = d_status + "Youtube URL: " + URL;
+                    var video = service.GetVideo(URL);
+                    label2.Text = d_status + "Your video is downloaded...saving to " + folder;
+                    string path = Path.Combine(folder, video.FullName);
+                    File.WriteAllBytes(path, video.GetBytes());
+                    label2.Text = d_status + "File is saved.";
+                    service.Dispose();
+                }
+            }
+            else
+            {
+                label2.Text = d_status + "Youtube link is invalid, please enter a new one.";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //do nothing
+        }
+
+        static string GetDefaultFolder()
+        {
+            var home = Environment.GetFolderPath(
+                Environment.SpecialFolder.UserProfile);
+
+            return Path.Combine(home, "Downloads");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            string URL = tb.Text;
+            if (URL.Trim().StartsWith("https://www.youtube.com/watch?v="))
+            {
+                label2.Text = d_status + "Valid youtube link!";
+            }
+            else
+            {
+                label2.Text = d_status + "Invalide youtube link!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.f_name.Text = "Set to: " + folderBrowserDialog1.SelectedPath;
+                this.folder = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+
+}
