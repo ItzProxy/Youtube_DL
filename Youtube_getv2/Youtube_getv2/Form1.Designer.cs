5d94d96a849d2c1ffd0abb07602a55585d91b783
ItzProxy <allure.success@gmail.com>
2016-02-08 03:23:54 -0600

    Working on a different version...still don't know how async works in this context

--- Youtube_getv2/Youtube_getv2/Form1.Designer.cs
+++ Youtube_getv2/Youtube_getv2/Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -43,6 +46,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Download";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -52,6 +56,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Browse";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -76,11 +81,44 @@
             this.textBox2.Size = new System.Drawing.Size(311, 20);
             this.textBox2.TabIndex = 4;
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 86);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Video";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(106, 85);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Audio";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(419, 228);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -100,6 +138,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Youtube_getv2/Youtube_getv2/Form1.cs
+++ Youtube_getv2/Youtube_getv2/Form1.cs
@@ -2,19 +2,112 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YoutubeExtractor;
 
 namespace Youtube_getv2
 {
     public partial class Form1 : Form
     {
+        string de_status = "Status: ";
+        string folder;
+
         public Form1()
         {
             InitializeComponent();
+            label1.Text = de_status + "Hello, welcome to the youtube extractor!";
+            folder = GetDefaultFolder();
+            textBox2.Text = folder;
+            label1.MaximumSize = new Size(438, 0); //wrap within window space
+            label1.AutoSize = true;
+            label2.MaximumSize = new Size(438, 0); //wrap within window space
+            label2.AutoSize = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string URL = textBox1.Text;
+                IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(URL, false); //get video formats
+                if (radioButton1.Checked)
+                {
+                    VideoInfo video = videoInfos
+                    .First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+                    if (video.RequiresDecryption)
+                    {
+                        DownloadUrlResolver.DecryptDownloadUrl(video); //decrypt signiture if needed
+                    }
+                    var videoDownloader = new VideoDownloader(video, Path.Combine(folder,
+                                                                                  RemoveIllegalPathCharacters(video.Title) + 
+                                                                                  video.VideoExtension));
+                    videoDownloader.DownloadProgressChanged += (se, args) => label1.Text = de_status + " Downloading - " + args.ProgressPercentage;
+                    videoDownloader.Execute();
+                    label2.Text = video.Title + " is saved to " + folder;
+                }
+                else if(radioButton2.Checked)
+                {
+                    VideoInfo audio = videoInfos
+                    .Where(info => info.CanExtractAudio)
+                    .OrderByDescending(info => info.AudioBitrate)
+                    .First();
+
+                    if (audio.RequiresDecryption)
+                    {
+                        DownloadUrlResolver.DecryptDownloadUrl(audio);
+                    }
+
+                    var audioDownload = new AudioDownloader(audio, Path.Combine(folder,
+                                                                                RemoveIllegalPathCharacters(audio.Title) + 
+                                                                                audio.AudioExtension));
+                    audioDownload.DownloadProgressChanged += (se, args) => label1.Text = de_status + " Downloading - " + args.ProgressPercentage;
+                    audioDownload.AudioExtractionProgressChanged += (se, args) => label1.Text = de_status + " Extracting audio - " + args.ProgressPercentage;
+
+                    audioDownload.Execute();
+
+                    label2.Text = audio.Title + " is saved to " + folder;
+                }
+                else
+                {
+                    textBox1.Text = de_status + " - Choose Video or Audio option -";
+                }
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine("Exception Message: " + err.Message);
+                label1.Text = de_status + " " + err.Message;
+            }
+        }
+        static string GetDefaultFolder()
+        {
+            var home = Environment.GetFolderPath(
+                Environment.SpecialFolder.UserProfile);
+
+            return Path.Combine(home, "Downloads");
+        }
+        private static string RemoveIllegalPathCharacters(string path)//remove /,",' and more from path title
+        {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(path, "");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if(folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                folder = folderDialog.SelectedPath;
+                textBox2.Text = folder;
+            }
         }
     }
+
 }
